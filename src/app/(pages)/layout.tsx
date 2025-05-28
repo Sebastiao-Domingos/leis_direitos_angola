3ab4e32aa9429c9d1656d9ff62581f2a6e2bf72e
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2025-05-28 15:33:11 +0100

    ola

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppHeader } from "@/components/section/header";
+import { AppSidebar } from "@/components/section/side-bar";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export function LayoutSCreen({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="w-full border">
+            <AppHeader />
+
+            {children}
+          </main>
+        </SidebarProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
+}
