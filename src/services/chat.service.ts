d29fdcf481f13bb76961bd2d5c3e609659da60e6
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2025-06-28 20:19:23 +0100

    adicionar os links

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -6,12 +6,12 @@ export interface ChatBody {
 }
 
 export class ChatService {
-  private readonly ENDPOINT = "webhook/da04f151-3438-46a5-8089-a7691f9271d3";
+  // private readonly ENDPOINT = "webhook/da04f151-3438-46a5-8089-a7691f9271d3";
 
   public async sendMessage(message: ChatBody): Promise<string> {
     // Simulate sending a message to the server and getting a response
     try {
-      const response = await api.post("webhook/automation", message);
+      const response = await api.post("webhook/automationtest", message);
       if (response.status === 200) {
         return response.data.response; // Assuming the response contains a 'response' field
       } else {
